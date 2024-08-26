47771c29b05bbb3fafc0ba75851bb3dbdd47eec8
AYAN HALDAR <119616417+AYAN10M@users.noreply.github.com>
2024-08-26 19:13:55 +0000

    DOM different HTML selector

--- 16_DOCUMENT_OBJECT_MODEL/lec-02/index.js
+++ 16_DOCUMENT_OBJECT_MODEL/lec-02/index.js
@@ -1,4 +1,19 @@
+// to select the element using tag name. Among all the tag the first one occuring would only be selected
 var title2 = document.querySelector("h2");
+
+// this is how we can change the html elements content. innerHTML is the property of HTML element h2
 title2.innerHTML = "Registration In Process";
 
-var submit_button = document.querySelector("button");
+// to select all the elements in the HTML file of the given tag name
+document.getElementsByTagName("li")[2].style.color = "yellow";
+
+// the given list of item will be converted to array in DOM, so to get the length of the array we need to
+document.getElementsByTagName("li").length;
+
+// to select all the elements in the HTML file of the given class name
+document.getElementsByClassName("list");
+
+// to select element of given id
+document.getElementById("submit_button").style.color = "green";
+
+// Note: Every id within HTML id tag should be unique
