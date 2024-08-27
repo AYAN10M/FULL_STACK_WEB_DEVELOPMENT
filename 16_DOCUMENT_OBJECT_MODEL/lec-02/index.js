94a0679effec80cf7a75cbe3af625ab6386433f4
AYAN HALDAR <119616417+AYAN10M@users.noreply.github.com>
2024-08-27 12:32:07 +0000

    Minor changes done

--- 16_DOCUMENT_OBJECT_MODEL/lec-02/index.js
+++ 16_DOCUMENT_OBJECT_MODEL/lec-02/index.js
@@ -1,6 +1,9 @@
 // to select the element using tag name. Among all the tag the first one occuring would only be selected
 var title2 = document.querySelector("h2");
 
+// using querySelector we can also select the HTML element using id
+document.querySelector("#title");
+
 // this is how we can change the html elements content. innerHTML is the property of HTML element h2
 title2.innerHTML = "Registration In Process";
 
